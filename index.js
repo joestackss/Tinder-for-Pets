78039e60ce02c6597a2e487bdeb50ca5c2b1c3e3
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-08 12:58:21 +0100

    html, css, js boilerplates

---
+++ index.js
@@ -0,0 +1,22 @@
+import Dog from './Dog.js'
+import dogsData from './data.js'
+let currentDogIndex = 0
+let currentDog = new Dog(dogsData[currentDogIndex])
+document.getElementById("accept-button").addEventListener('click', yes)
+
+render()
+
+function render() {
+    document.getElementById('card').innerHTML = currentDog.getDogHtml()
+}
+
+function getNewDog() {
+    currentDogIndex+=1
+    currentDog = new Dog(dogsData[currentDogIndex])
+    render()
+}
+
+function yes() {
+    currentDog.setMatchStatus(true)
+    getNewDog()
+}
